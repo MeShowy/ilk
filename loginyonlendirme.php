9f219b5eeb44919b9c897107cea32344f91d6dfa
bberatb <beratbahadur@outlook.com>
2024-05-16 22:16:23 +0300

    Login Sayfa tamamlandı. Ana sayfa yapıldı.

--- loginyonlendirme.php
+++ loginyonlendirme.php
@@ -1,28 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
+    <html lang="en">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Giriş Yap</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  
     <style>
-.footer {
-   position: fixed;
-   left: 0;
-   bottom: 0;
-   width: 100%;
-  
-  
-   text-align: center;
-}
+    footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        
+        
+        
+       
+    }
 </style>
-</head>
-<body>
-  
 
-<!--Navbar Kodları-->
+<body>
 <div>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="anasayfa.html" style="padding-left: 15px;">Ana Sayfa</a>
@@ -72,27 +67,49 @@
       
 
   </div>
-</div>
+<?php
+$email = $_POST["email"];
+$sifre = $_POST["sifre"];
+
+if ( $email == "b231210072@sakarya.edu.tr" && $sifre == "b231210072") {
+    
+    echo  '
+    <head>
+        <meta http-equiv="refresh" content="5; URL=anasayfa.html" />
+        <title>Hoşgeldiniz</title>
+    </head>
+    
+    
+     Merhaba, 
+     
+   ' ;
+    
+    
+    echo strtok($_POST["email"], '@');
+    echo " 5 Saniye İçinde Ana Sayfaya Yönlendiriliyorsunuz...";
+}
+
+else  {
+echo
+    '
+    <head>
+        <meta http-equiv="refresh" content="5; URL=loginsayfa.php" />
+       
+        <title>Başarısız Giriş</title>
+    </head>
+    <body>
+        Başarısız Giriş. 5 Saniye İçinde Giriş Sayfasına Tekrar Yönlendiriliyorsunuz...
+    </body>
+    
+    ';
 
 
-<!--Form-->
-<form action="loginyonlendirme.php" method="post" style="width: 50%; margin:auto; margin-top:15%;">
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email adresi</label>
-    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Emailinizi Giriniz">
-    <small id="emailHelp" class="form-text text-muted">Emailiniz güvendedir.</small>
-  </div>
-  <div style="margin-top: 20px;" class="form-group">
-    <label for="exampleInputPassword1">Şifre</label>
-    <input type="password" name="sifre" class="form-control" id="exampleInputPassword1" placeholder="Şifrenizi giriniz">
-  </div>
-  
-  <button style="margin-top: 20px;" type="submit" class="btn btn-primary">Gönder</button>
-</form>
-
+}
+?>
+ 
 
 <!-- Footer -->
-<div class="footer" style="display:relative; bottom: 0px;  ">
+<div style="display:block; bottom:0px;">
         <footer style="width: 100%;" id="sticky-footer" class=" py-4 bg-dark text-white-50" >
           <div class="container text-center">
             <h4 style="color: rgb(255, 255, 255);">Berat Bahadur</h4><br>
@@ -110,6 +127,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     
-</body>
-</html>
 
+</body>
+
+
+</html>
